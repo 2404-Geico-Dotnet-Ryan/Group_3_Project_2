d7cc162b61c5a0e90ca47d7de59606a3ba0f38cc
AWard5683 <amandafward@hotmail.com>
2024-06-05 17:06:49 -0400

    EF and Model creation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Project2.Models;
+using Project2.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddScoped<IUserService, UserService>();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 
 var app = builder.Build();
 
